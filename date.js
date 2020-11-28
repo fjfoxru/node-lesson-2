dff9599063db7b2b5537f51e97e65f985a35835f
fjfoxru <61179622+fjfoxru@users.noreply.github.com>
2020-11-28 20:46:40 +0300

    Update date.js

--- date.js
+++ date.js
@@ -1,13 +1,49 @@
 #!/usr/bin/env node
 var argv = require('yargs/yargs')(process.argv.slice(2)).argv;
 
-const data = new Date();
+const command = argv._[0];
 
-if (argv._.find(el => el === 'current')) {
-    console.log(data);
-}
 
-if (argv.year) {
-    console.log(data.getFullYear);
-}
+let requestDate;
 
+    switch(command) {
+        case 'current': 
+            const { date, month, year } = argv; 
+            requestDate = new Date();
+            if (year) requestDate = requestDate.getFullYear()
+            if (month) requestDate = requestDate.getMonth()
+            if (date) requestDate = requestDate.getDate()
+        break;
+      
+        case 'add':
+            if (argv.d) {
+                requestDate = new Date();
+                requestDate = requestDate.getDate() + argv.d
+            } else if (argv.m) {
+                requestDate = new Date();
+                requestDate = requestDate.getMonth() + argv.m
+            } else if (argv.y) {
+                requestDate = new Date();
+                requestDate = requestDate.getFullYear() + argv.m
+            }
+        break;
+
+        case 'sub':
+            if (argv.d) {
+                requestDate = new Date();
+                requestDate = requestDate.getDate() - argv.d
+            } else if (argv.m) {
+                requestDate = new Date();
+                requestDate = requestDate.getMonth() - argv.m
+            } else if (argv.y) {
+                requestDate = new Date();
+                requestDate = requestDate.getFullYear() - argv.m
+            }
+        break; 
+
+      }
+
+
+
+
+console.log(requestDate);
